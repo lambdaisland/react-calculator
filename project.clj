6d307d524f08706b5379f7888f204813721932f8
Arne Brasseur <arne@arnebrasseur.net>
2016-05-28 10:11:57 +0200

    Add doo so lein cljsbuild once does not complain

--- project.clj
+++ project.clj
@@ -90,7 +90,8 @@
              {:dependencies [[figwheel "0.5.2"]
                              [figwheel-sidecar "0.5.2"]
                              [com.cemerick/piggieback "0.2.1"]
-                             [org.clojure/tools.nrepl "0.2.12"]]
+                             [org.clojure/tools.nrepl "0.2.12"]
+                             [doo "0.1.6"]]
 
               :plugins [[lein-figwheel "0.5.2"]
                         [lein-doo "0.1.6"]]
